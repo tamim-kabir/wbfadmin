aebd66e0501f229e04058890568086c1ad4fdef7
AKTamim <mdahasanulkabir6@gmail.com>
2023-01-30 00:44:26 +0600

    aaa

--- assets/js/config.js
+++ assets/js/config.js
@@ -222,7 +222,7 @@ function loadInterast() {
                                 <td>${++sl}</td>
                                 <td>${snapshot.val().name}</td>
                                 <td>${format}</td>
-                                <td><img src="${snapshot.val().iconUri}" alt="${snapshot.val().iconName}" width="10" height="10"></td>
+                                <td class="d-flex justify-content-center"><img src="${snapshot.val().iconUri}" alt="${snapshot.val().iconName}" width="30" height="30"></td>
                                 <td class="d-flex justify-content-center">
                                     <a href="javascript:void(0)" data-id="${snapshot.key}" class="remove"><i class="las la-trash text-danger font-16"></i></a>
                                     <a href="javascript:void(0)" data-id="${snapshot.key}" class="pl-2 edit"><i class="las la-pen text-secondary font-16"></i></a>                                                            
